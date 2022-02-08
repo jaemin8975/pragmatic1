1dc41d02f289d60fad2c9f8bb4c3e17cfb8531e5
jaemin8975 <thdwpals1234@naver.com>
2022-02-09 06:01:57 +0900

    django 15

--- accountapp/views.py
+++ accountapp/views.py
@@ -40,10 +40,12 @@ class AccountDetailView(DetailView):
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
