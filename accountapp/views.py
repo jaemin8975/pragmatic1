06cbabdd0a4009c190d93426f14a74c4de9323e4
jaemin8975 <thdwpals1234@naver.com>
2022-02-09 02:22:51 +0900

    django 14

--- accountapp/views.py
+++ accountapp/views.py
@@ -25,7 +25,7 @@ def hello_world(request) :
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_output':hello_world_list})
 
-class AccountCreateView(CreateView) :
+class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
