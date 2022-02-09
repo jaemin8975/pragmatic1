8d20d20a8da11e8f0e85f108492b1c360c00eb7e
jaemin8975 <thdwpals1234@naver.com>
2022-02-09 09:57:41 +0900

    django 18

--- profileapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from profileapp.models import Profile
-
-
-class ProfileCreateForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['image', 'nickname', 'message']
